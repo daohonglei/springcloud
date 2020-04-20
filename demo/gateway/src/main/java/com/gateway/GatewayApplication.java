7394bd6b55a661cad4b3822944e37bb274cf5728
daophonglei <858883986@qq.com>
2020-04-20 22:28:52 +0800

    profile
    profile

--- demo/gateway/src/main/java/com/gateway/GatewayApplication.java
+++ demo/gateway/src/main/java/com/gateway/GatewayApplication.java
@@ -2,15 +2,27 @@ package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class GatewayApplication {
+public class GatewayApplication extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        ConfigurableApplicationContext application=springApplicationBuilder.context();
+        ConfigurableEnvironment configurableEnvironment=application.getEnvironment();
+        String string=configurableEnvironment.getProperty("os.name");
+        System.out.println(string);
+        configurableEnvironment.setActiveProfiles();
+        return springApplicationBuilder.sources(GatewayApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
